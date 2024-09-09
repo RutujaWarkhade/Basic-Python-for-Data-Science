bc1b9494ea97d2d49f82bb75bf79c6f42eb329e6
Rutuja Shivaji Warkhade <167538733+RutujaWarkhade@users.noreply.github.com>
2024-09-09 22:26:47 +0530

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 21:12:16 2024
+
+@author: OM
+"""
+
+print("Hello World!")
+x=1.5
+print(x)
+print(type(x))
+x=100000001
+print(x)
+print(type(x))
